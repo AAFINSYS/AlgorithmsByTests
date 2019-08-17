636565bed96c74a559828a9e63ab200b72906260
AAFINSYS <43729299+aafinsys@users.noreply.github.com>
2019-08-17 11:20:44 +0200

    iterative version for bibonacci

--- Fibonacci.py
+++ Fibonacci.py
@@ -67,5 +67,43 @@ class TestTailRecursiveFibonacci(unittest.TestCase):
         self.assertEqual(tail_recursive_fibonacci(15), tail_recursive_fibonacci(14) + tail_recursive_fibonacci(13))
 
 
+def iterative_fibonacci(input_number):
+    if input_number in (0, 1):
+        return input_number
+
+    value_fibo_n_minus_one = 1
+    value_fibo_n_minus_two = 0
+
+    for i in range(2, input_number+1):
+        value_fibo_n_minus_two, value_fibo_n_minus_one = value_fibo_n_minus_one, value_fibo_n_minus_one + value_fibo_n_minus_two
+
+    return value_fibo_n_minus_one
+
+
+class TestIterativeFibonacci(unittest.TestCase):
+    def test_should_return_zero_when_input_is_zero(self):
+        actual = iterative_fibonacci(0)
+
+        self.assertEqual(actual, 0)
+
+    def test_should_return_one_when_input_number_is_one(self):
+        actual = iterative_fibonacci(1)
+
+        self.assertEqual(actual, 1)
+
+    def test_should_return_one_when_input_number_is_two(self):
+        actual = iterative_fibonacci(2)
+
+        self.assertEqual(actual, 1)
+
+    def test_should_return_610_when_input_number_is_15(self):
+        actual = iterative_fibonacci(15)
+
+        self.assertEqual(actual, 610)
+
+    def test_should_return_same_result_for_15_when_processing_sum_from_the_results_from_13_and_14(self):
+        self.assertEqual(iterative_fibonacci(15), iterative_fibonacci(14) + iterative_fibonacci(13))
+
+
 if __name__ == "__main__":
     unittest.main()
--- main_tests_algorithms.py
+++ main_tests_algorithms.py
@@ -38,5 +38,74 @@ class TestsStringAlgorithmConvertingNumbersToStrings(unittest.TestCase):
         self.assertEqual(actual, -1234)
 
 
+def process_integer_to_string(param):
+    return chr(param + ord("0"))
+
+
+class TestStringAlgorithmConvertingNumbersToStrings(unittest.TestCase):
+    def test_should_return_string_one_when_number_one_is_processed(self):
+        actual = process_integer_to_string(1)
+
+        self.assertEqual(actual, "1")
+
+    def test_should_return_string_five_when_number_five_is_processed(self):
+        actual = process_integer_to_string(5)
+
+        self.assertEqual(actual, "5")
+
+    @unittest.skip("skip")
+    def test_should_return_string_fifteen_when_number_fifteen_is_processed(self):
+        actual = process_integer_to_string(15)
+
+        self.assertEqual(actual, "15")
+
+
+def even_first_sort_array(array_to_sort):
+    first_even_position_to_inspect = 0
+    first_odd_position_to_inspect = len(array_to_sort) - 1
+
+    while first_even_position_to_inspect < first_odd_position_to_inspect:
+        if array_to_sort[first_even_position_to_inspect] % 2 == 0:
+            first_even_position_to_inspect += 1
+        else:
+            array_to_sort[first_even_position_to_inspect], array_to_sort[first_odd_position_to_inspect] = \
+                array_to_sort[first_odd_position_to_inspect], array_to_sort[first_even_position_to_inspect]
+
+            first_odd_position_to_inspect -= 1
+
+
+class TestArrayBootcamp(unittest.TestCase):
+    def test_should_return_even_first_sorted_array_when_two_element_integer_array_is_processed(self):
+        actual = [1, 2]
+
+        even_first_sort_array(actual)
+
+        self.assertListEqual(actual, [2, 1])
+
+    def test_should_return_even_first_sorted_array_when_three_element_integer_array_is_processed(self):
+        actual = [1, 2, 3]
+
+        even_first_sort_array(actual)
+
+        self.assertListEqual(actual, [2, 3, 1])
+
+    def test_should_return_even_first_sorted_array_when_eleven_element_integer_array_is_processed(self):
+        actual = [1, 2, 0, 7, 9, 11, 5, 6, 8, 3, 12]
+
+        even_first_sort_array(actual)
+
+        self.assertListEqual(actual, [12, 2, 0, 8, 6, 5, 11, 9, 3, 7, 1])
+
+
+class TestArray_MaxProfitFromBuyOnceSellOnce(unittest.TestCase):
+    def should_return_profit_when_price_array_contains_two_prices(self):
+        actual = 0
+
+        priceArray = [200, 220]
+        actual = 20
+
+        self.assertEqual(actual, 20)
+
+
 if __name__ == "__main__":
     unittest.main()
